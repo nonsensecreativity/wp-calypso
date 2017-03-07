bf59aaf83cc6d98cf3486a8ef39d307e03e1642e
James Tien <southp0105@gmail.com>
2017-03-07 17:55:55 +0800

    Rename reset-password-email-sent to reset-password-email-form

--- client/account-recovery/controller.js
+++ client/account-recovery/controller.js
@@ -11,6 +11,7 @@ import LostPasswordPage from 'account-recovery/lost-password';
 import ForgotUsernamePage from 'account-recovery/forgot-username';
 import ResetPasswordPage from 'account-recovery/reset-password';
 import ResetPasswordForm from 'account-recovery/reset-password/reset-password-form';
+import ResetPasswordEmailForm from 'account-recovery/reset-password/reset-password-email-form';
 import TransactionIdForm from 'account-recovery/reset-password/transaction-id-form';
 import { getCurrentUser } from 'state/current-user/selectors';
 
@@ -28,6 +29,16 @@ export function resetPassword( context, next ) {
 	context.primary = (
 		<ResetPasswordPage basePath={ context.path }>
 			<ResetPasswordForm />
+		</ResetPasswordPage>
+	);
+
+	next();
+}
+
+export function resetPasswordEmailForm( context, next ) {
+	context.primary = (
+		<ResetPasswordPage basePath={ context.path }>
+			<ResetPasswordEmailForm />
 		</ResetPasswordPage>
 	);
 
--- client/account-recovery/index.js
+++ client/account-recovery/index.js
@@ -5,6 +5,7 @@ import {
 	lostPassword,
 	forgotUsername,
 	resetPassword,
+	resetPasswordEmailForm,
 	resetPasswordByTransactionId,
 	redirectLoggedIn
 } from './controller';
@@ -16,6 +17,7 @@ export default function( router ) {
 		router( '/account-recovery', redirectLoggedIn, lostPassword );
 		router( '/account-recovery/forgot-username', redirectLoggedIn, forgotUsername );
 		router( '/account-recovery/reset-password', redirectLoggedIn, resetPassword );
+		router( '/account-recovery/reset-password/email-form', redirectLoggedIn, resetPasswordEmailForm );
 		router( '/account-recovery/reset-password/transaction-id', redirectLoggedIn, resetPasswordByTransactionId );
 	}
 }
